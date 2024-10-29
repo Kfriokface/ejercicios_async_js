688d358b584f4b2c70a651ef3a45fbdcbfb016e5
Alberto Sancho <designalbertosancho@gmail.com>
2024-10-30 00:45:26 +0100

    ejercicio 1

--- ejercicio1/index.js
+++ ejercicio1/index.js
@@ -1,18 +1,39 @@
 const base_url = "https://thronesapi.com";
 
+const selectElement = document.querySelector('#character-list');
+const imgContainer = document.querySelector('.character-image');
+
 fetch(base_url + '/api/v2/Characters')
 .then(data => data.json())
 .then(data => {
-  console.log(data);
+  // Pongo la imagen inicial
+  imgContainer.setAttribute('src', data[0].imageUrl)
+  // LLamo a las funciones
+  getCharacters(data);
+  showImage(data);
+})
+.catch(console.error('No ha sido posible la conexión')
+);
+
+const getCharacters = (data) => {
   data.forEach(character => {
-    const option = document.querySelector("#character-list").appendChild(document.createElement("option"));
+    const option = selectElement.appendChild(document.createElement("option"));
     option.setAttribute('value', character.id)
     option.innerHTML = `
      ${character.firstName} ${character.lastName}
     `;
   });
-});
+};
 
+const showImage = (data) => {
+  selectElement.addEventListener('change', (event) => {
+    const selectedId = event.target.value;
+    const selectedItem = data.find(character => character.id === parseInt(selectedId));
+    if (selectedItem) {
+      imgContainer.setAttribute('src', selectedItem.imageUrl);
+    }
+  })
+};
 
 
 
